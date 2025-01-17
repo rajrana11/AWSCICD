ebdb7293d106b0ce721db34ac53d5347fe059eb2
rajrana11 <rajrana11@gmail.com>
2025-01-17 17:44:32 +0530

    Add project files.

---
+++ AWSCICD/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+//builder.Services.AddControllers();
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+
+
+app.Run();
